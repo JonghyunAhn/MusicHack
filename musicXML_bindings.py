624fce53e6aa5095591ff4da5a6cd46f3b3b77cd
Leo Wu <leowu@Leos-MacBook-Pro.local>
2016-04-02 04:52:46 -0700

    semi-tested bindings also Kangaroo Jack was a wife-beater

--- musicXML_bindings.py
+++ musicXML_bindings.py
@@ -17,10 +17,18 @@ class Score:
         self.measures = []
 
     def add_measure(self, measure):
+        if self.current_measure == 1:
+            measure.set_first_measure(self.beats, self.beat_type)
+        measure.set_measure_number(self.current_measure)
         self.measures.append(measure)
+        self.current_measure += 1
 
     def get_xml(self):
-        pass
+        return '<score-partwise version="3.0">\n\t<part-list>\n\t\t<score-part id="P0">\n\t\t\t<part-name>{}</part-name>\n\t\t</score-part>\n\t</part-list>\n\t<part id="P0">'.format(self.name) + "".join([i.get_xml() for i in self.measures]) + "\n\t</part>\n</score-partwise>"
+
+    def write_to_file(self):
+        with open(self.name, 'w') as f:
+            f.write(self.get_xml())
 
     def __str__(self):
         s = "Score: "+ self.name + " " +  str(self.beats) + " " +  str(self.beat_type) + "\n"
@@ -31,12 +39,19 @@ class Score:
 class Measure:
     def __init__(self):
         self.elements = []
+        self.first_measure_string = "\n\t\t\t<attributes>\m\t\t\t\t<divisions>1</divisions>\n\t\t\t</attributes>"
 
     def add_element(self, element):
         self.elements.append(element)
 
+    def set_measure_number(self, number):
+        self.number = number
+
+    def set_first_measure(self, beats, beat_type):
+        self.first_measure_string = "\n\t\t\t<attributes>\n\t\t\t\t<divisions>1</divisions\n\t\t\t\t<time>\n\t\t\t\t\t<beats>{}</beats>\n\t\t\t\t\t<beat-type>{}</beat-type>\n\t\t\t\t</time>\n\t\t\t\t<clef>\n\t\t\t\t\t<sign>G</sign>\n\t\t\t\t\t<line>2</line>\n\t\t\t\t</clef>\n\t\t\t</attributes>".format(beats, beat_type)
+
     def get_xml(self):
-        pass
+        return '\n\t\t<measure number="{}">{}'.format(self.number, self.first_measure_string) + "".join([i.get_xml() for i in self.elements]) + "\n\t\t</measure>"
 
     def __str__(self):
         s = "\tMeasure: "
@@ -78,7 +93,7 @@ class Note:
         self.octave = octave
         self.note_length = note_length
         self.dot_string = "\n\t\t\t\t<dot/>" if dot else ""
-        self._chord_string = ""
+        self.chord_string = ""
         if semi == "flat":
             self.semi = -1
         elif semi == "double flat":
@@ -91,15 +106,16 @@ class Note:
             self.semi = 0
 
     def set_chord(self):
-        self._chord_string = "\n\t\t\t\t<chord/>"
+        self.chord_string = "\n\t\t\t\t<chord/>"
 
     def get_xml(self):
-        return "\n\t\t\t<note>{}\n\t\t\t\t<pitch>\n\t\t\t\t\t<step>{}</step>\n\t\t\t\t\t<octave>{}</octave>\n\t\t\t\t\t<alter>{}</alter>\n\t\t\t\t</pitch>\n\t\t\t\t<type>{}</type>{}\n\t\t\t</note>".format(self._chord_string, self.pitch, self.octave, self.semi, self.note_length, self.dot_string)
+        return "\n\t\t\t<note>{}\n\t\t\t\t<pitch>\n\t\t\t\t\t<step>{}</step>\n\t\t\t\t\t<octave>{}</octave>\n\t\t\t\t\t<alter>{}</alter>\n\t\t\t\t</pitch>\n\t\t\t\t<type>{}</type>{}\n\t\t\t</note>".format(self.chord_string, self.pitch, self.octave, self.semi, self.note_length, self.dot_string)
 
     def __str__(self):
         if self.dot_string != "":
             return self.pitch + str(self.octave) + "-" + self.note_length + "*"
         return self.pitch + str(self.octave) + "-" + self.note_length
+
 class Rest:
     def __init__(self, note_length, dot=False):
         self.note_length = note_length
